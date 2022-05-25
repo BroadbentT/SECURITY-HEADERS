9cb600e19d88c00930cab780759ce097504565f4
root <root@kali.Domain>
2022-05-25 21:12:22 +0100

    HEADERS

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # SECURITY-HEADERS
 
-security_headers.py   : 18405a947e05cbe2cc46d2748c40c87d<br>
-depreciated_headers.py: a0812c886d045583375e118ad58f0724
-
-Usage: python3 security_headers.py google.com<br>
-       python3 depreciated_headers.py google.com
-       
+|security_headers.py   |18405a947e05cbe2cc46d2748c40c87d|
+|depreciated_headers.py|a0812c886d045583375e118ad58f0724|
+|insecure_methods.py   |b8e983e4d91bf6916c48b5ee7a3d46e9|
+<br>
+|Usage: |python3 security_headers.py google.com   |
+|       |python3 depreciated_headers.py google.com|
+|       |python3 insecure_methods.py google.com   |       
 <br>
 <br>
-
 <p align="center">
   <img src="https://github.com/BroadbentT/SECURITY-HEADERS/blob/main/headers1.png">
   <img src="https://github.com/BroadbentT/SECURITY-HEADERS/blob/main/headers2.png">  
---
+++ insecure_methods.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python3
+# coding:UTF-8
+
+# -------------------------------------------------------------------------------------
+#          PYTHON SCRIPT FILE FOR THE CONFIRMATION OF HTTP SECURITY HEADERS
+#                       BY TERENCE BROADBENT BSc CYBER SECURITY
+# -------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------- 
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub
+# Version : 1.0                                                               
+# Details : Load required imports.
+# Modified: N/A
+# -------------------------------------------------------------------------------------
+
+import os
+import sys
+import requests
+
+from termcolor import colored # pip3 install termcolor  
+
+colour1 = 'green'
+colour2 = 'yellow'
+colour3 = 'blue'
+colour4 = 'red'
+colour5 = 'cyan'
+
+# -------------------------------------------------------------------------------------
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub                                                               
+# Version : 1.0                                                               
+# Details : Conduct simple and routine tests on any user supplied arguements.
+# Modified: N/A                                                               
+# -------------------------------------------------------------------------------------
+
+if os.geteuid() != 0:
+   print("\nPlease run this python script as root...")
+   exit(True)
+
+if len(sys.argv) < 2:
+   print("Use the command python3 securityheaders.py pentestpeople.co.uk...")
+   exit()
+host = sys.argv[1]
+
+os.system("xdotool key Alt+Shift+S; xdotool type 'HTTP SECURITY HEADERS'; xdotool key Return")
+    
+# -------------------------------------------------------------------------------------
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub                                                               
+# Version : 1.0
+# Details : Create functional subroutine calls from main.
+# Modified: N/A                                                               
+# -------------------------------------------------------------------------------------
+
+def scanhost(filename, host, port):
+   os.system("nmap -p " + str(port) + " --script=http-methods -oN " + filename + " " + host + " 2>&1 > scan.tmp")
+   return   
+
+def parsehost(file):
+   a = 0
+   b = 0
+   c = 0
+   d = 0
+   e = 0
+   f = 0
+   g = 0
+   h = 0
+   with open(file) as search:
+      for line in search:
+         if ("GET".upper() in line.upper()): 
+            print(colored("GET: Found", colour1))
+            a = 1
+         if ("HEAD".upper() in line.upper()): 
+            print(colored("HEAD: Found", colour4))
+            b = 1
+         if ("POST".upper() in line.upper()): 
+            print(colored("POST: Found", colour1))
+            c = 1
+         if ("PUT".upper() in line.upper()): 
+            print(colored("PUT: Found", colour4)) 
+            d = 1
+         if ("DELETE".upper() in line.upper()): 
+            print(colored("DELETE: Found", colour4)) 
+            e = 1
+         if ("CONNECT".upper() in line.upper()): 
+            print(colored("CONNECT: Found", colour4)) 
+            f =1
+         if ("OPTIONS".upper() in line.upper()): 
+            print(colored("OPTIONS: Found", colour1)) 
+            g = 1
+         if ("TRACE".upper() in line.upper()): 
+            print(colored("TRACE: Found", colour4)) 
+            h = 1                        
+   if a == 0:
+      print(colored("GET: Not Found", colour2))
+   if b == 0:
+      print(colored("HEAD: Not Found", colour2))
+   if c ==  0:
+      print(colored("POST: Not Found", colour2))
+   if d == 0:
+      print(colored("PUT: Not Found", colour2))
+        
+   if e == 0:
+      print(colored("DELETE: Not Found", colour2))        
+   if f == 0:
+      print(colored("CONNECT: Not Found", colour2))        
+   if g == 0:
+      print(colored("OPTIONS: Not Found", colour2))        
+   if h == 0:
+      print(colored("TRACE: Not Found", colour2))       
+   return
+   
+def scanpage1(host):
+   method_list = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE']
+   for method in method_list:
+      req = requests.request(method, "http://" + host)
+      print(colored(method, colour3), end=' ')
+      print(colored(req.status_code, colour3), end=' ')
+      print(colored(req.reason, colour3)) 
+   return
+   
+def scanpage2(host):
+   method_list = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE']
+   for method in method_list:
+      req = requests.request(method, "https://" + host)
+      print(colored(method, colour3), end=' ')
+      print(colored(req.status_code, colour3), end=' ')
+      print(colored(req.reason, colour3))     
+   return
+   
+def banner():
+   print(colored("\nPentest People - HTTP Insecure Methods Checker.", colour5))
+   print("Target:", host)   
+   print("- - - - - - - - - - - - - - - - - - - - - - -")   
+   return
+   
+# -------------------------------------------------------------------------------------
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub                                                               
+# Version : 1.0
+# Details : MAIN PROGRAM.
+# Modified: N/A                                                               
+# -------------------------------------------------------------------------------------
+
+os.system("clear")
+banner()
+print("HOST CHECK PORT 80:")
+scanhost("insecuremethods1.txt", host, 80)
+parsehost("insecuremethods1.txt")
+print("WEBPAGE CHECK:")
+scanpage1(host)
+banner()
+print("HOST CHECK PORT 443:")
+scanhost("insecuremethods2.txt", host, 443)
+parsehost("insecuremethods2.txt")
+print("WEBPAGE CHECK:")
+scanpage2(host)
+print("\n")
+
